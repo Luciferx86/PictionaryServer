9fb22bcda87481d584f8bab3aed126f4a2824459
luciferx86 <akshayarora.10.6.97@gmail.com>
2020-08-02 14:26:59 +0530

    not wokring

--- calling.js
+++ calling.js
@@ -1,12 +1,14 @@
 var io = require('socket.io-client')
 // var socket = io.socket("http://localhost:3000/")
 console.log("socket init");
-var socket = io.connect("http://127.0.0.1:3000");
+var socket = io.connect("https://pictionary-server.herokuapp.com");
 console.log("socket init");
 socket.connect();
 console.log("socket connected");
-socket.emit("touch",0,0);
-socket.off();
-socket.disconnect();
+for (var i = 0; i < 100; i++) {
+    socket.emit("touch", i+3, i*3 + i);
+}
+// socket.off();
+// socket.disconnect();
 console.log("socket emitted");
 // socket.emit("touch",0,0);
